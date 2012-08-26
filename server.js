bb7e33ce31dc60be741d82941af31676992d342e
Alex Scotton <alex@alexscotton.com>
2012-08-26 13:19:54 +0100

    Changed watch library (Watchr) latency improvements, no double events fired

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 console.log('NodeJS CodeKit / Web Dev Toolkit');
 console.log('Watching: ' + process.cwd());
 
-var watch = require('watch');
+// Require
+var watchr = require('watchr')
 
-watch.createMonitor(process.cwd(), function (monitor) {
-	//monitor.files[process.cwd() + '/.zshrc'] // Stat object for my zshrc.
-	monitor.on("created", function (f, stat) {
-		// Handle file changes
-		console.log('New File Created: ' + f);
-		console.log(stat);
-	});
-	monitor.on("changed", function (f, curr, prev) {
-		// Handle new files
-		console.log('File Changed: ' + f);
-		console.log(curr);
-		console.log(prev);
-	});
-	monitor.on("removed", function (f, stat) {
-		// Handle removed files
-		console.log('File Deleted: ' + f);
-		console.log(stat);
-	});
+// Watch a directory or file
+watchr.watch({
+    path: process.cwd(),
+    listener: function(eventName,filePath,fileCurrentStat,filePreviousStat) {
+        console.log('a watch event occured:',arguments);
+    },
+    next: function(err,watcher) {
+        if (err)  throw err;
+        console.log('watching setup successfully');
+    }
 });
-
-testing
