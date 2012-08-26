fd15d8325f85519d252ee4ab14b82b0b8ad7cded
Alex Scotton <alex@alexscotton.com>
2012-08-26 18:11:59 +0100

    Browser Refresh | LESS Compiler | Example Client
    * Socket.IO functionaility for sending commands to connected clients
    * WatchEvents beginning to detect type of file, compiling (not
    outputing) less files to css (need to determine best course for only
    compiling main less file on any less file change :S)
    * Example Client, with first parts of an inline code snippet for using
    this! (simple)

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+console.log('--------------------------------');
 console.log('NodeJS CodeKit / Web Dev Toolkit');
+console.log('--------------------------------');
 console.log('Watching: ' + process.cwd());
 
 // Require the watch module
-var watchr = require('watchr')
+var watchr  = require('watchr')
+  , fs 		= require('fs')
+  , less 	= require('less')
+  , io 		= require('socket.io').listen(8080);
+
+// In-Line Config - Soon to move to external json object, or argv
 var config = {
 	color : true
 };
+io.set('log level', 0);
+
 
 // Watch the current working directory
 watchr.watch({
@@ -20,27 +29,85 @@ watchr.watch({
     }
 });
 
+io.sockets.on('connection', function (socket) {
+	console.log(color('[New Display [' + socket.id + '] on Socket.IO]', 'bold'));
+});
+
+
+/* PROCESSING FUNCTIONS */
+
+// MAIN 'SORTING' PROCESS
 function event_processor(arg) {
+	/*var filename = arg[1].replace(process.cwd(), "");
+	var directory = arg[1].replace(filename, "");*/
+
+	var filename = arg[1].substr(arg[1].lastIndexOf('\\') + 1);
+	var directory = arg[1].slice(0,arg[1].lastIndexOf('\\'));
+	var ext = filename.substr(filename.lastIndexOf(".") + 1);
+
 	switch(arg[0]) {
 		case "new":
 			//New File
 			console.log(color('  A file has been created:', 'green'));
-			console.log(color('    FILE       : ', 'cyan') + arg[1]);
+			console.log(color('    DIRECTORY  : ', 'cyan') + directory);
+			console.log(color('    FILE       : ', 'cyan') + filename);
+			console.log(color('    EXTENSION  : ', 'cyan') + ext);
 			console.log(color('    SIZE(bytes): ', 'cyan') + arg[2].size);
+			io.sockets.emit('update', { type: 'refresh' });
 		break;
 		case "unlink":
 			//delete File
 			console.log(color('  A file has been deleted:', 'red'));
-			console.log(color('    FILE       : ', 'cyan') + arg[1]);
+			console.log(color('    DIRECTORY  : ', 'cyan') + directory);
+			console.log(color('    FILE       : ', 'cyan') + filename);
+			console.log(color('    EXTENSION  : ', 'cyan') + ext);
+			io.sockets.emit('update', { type: 'refresh' });
 		break;
 		case "change":
 			//change File
 			console.log(color('  A file has been changed:' , 'magenta'));
-			console.log(color('    FILE       : ', 'cyan') + arg[1]);
+			console.log(color('    DIRECTORY  : ', 'cyan') + directory);
+			console.log(color('    FILE       : ', 'cyan') + filename);
+			console.log(color('    EXTENSION  : ', 'cyan') + ext);
 			console.log(color('    SIZE(bytes): ', 'cyan') + arg[3].size);
-		break;
+			switch(ext) {
+				default:
+					io.sockets.emit('update', { type: 'refresh' });	
+				break;
+				case 'css':
+					io.sockets.emit('update', { type: 'css' });
+				break;
+				case 'less':
+					fs.readFile(arg[1], 'ascii', function read(err, data) {
+						if (err) {
+							throw err;
+						}
+						compile_less(directory, filename, data);
+					});
+			}
 	}
 }
+
+// LESS COMPILER
+function compile_less(directory, filename, file) {
+	console.log('dir:' + directory);
+	console.log('filename:' + filename);
+	console.log('extracted less:' + file);
+
+	/*less.render(file, function (e, css) {
+		console.log(css);
+	});*/
+	var parser = new(less.Parser)({
+		paths: [directory],
+		filename: filename
+	});
+	parser.parse(file, function (err, tree) {
+		if (err) { return console.log(err) }
+		console.log('[OUTPUT CSS]');
+		console.log(tree.toCSS({compress:false})); // CSS output
+	});
+}
+
 
 /* CONSOLE LOG TERMINAL ANSI COLORS */
 function color(str, color) {
